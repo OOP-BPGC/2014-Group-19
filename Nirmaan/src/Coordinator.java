1b4f1c94b4ea23ce68b16b451ef45866101ae64e
Rajat Gupta <f2013464@goa.bits-pilani.ac.in>
2014-11-13 15:41:26 +0530

    Update Coordinator.java

--- Nirmaan/src/Coordinator.java
+++ Nirmaan/src/Coordinator.java
@@ -27,4 +27,40 @@ public class Coordinator extends Student {
 	public boolean closeRegistration(Event evt) {
 
 	}
+	
+	
+	public void promote(Event event) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Nirmaan Event!!! --" + event.name);
+			System.out.println("Starts on " + event.startDate + "\t Ends on "+ event.endDate);
+			System.out.println("Do attend the event!!!!");	
+		}	
+	}
+	
+	public String search(String key) {
+
+		Pattern pat;
+		Matcher mat;
+		String result = "";
+		pat = Pattern.compile(".*" + key + ".*");
+
+		try {
+			FileInputStream fstream = new FileInputStream("file.txt");
+			DataInputStream ind = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(ind));
+			String strLine;
+			while ((strLine = br.readLine()) != null) {
+				mat = pat.matcher(strLine);
+				boolean found = mat.matches();
+				if (found) {
+					result +=('\n'+strLine);
+				}
+			}
+			ind.close();
+		} catch (IOException  e) {
+			return "Error: " + e.getMessage();
+		}
+		return result;
+	}
+	
 }
