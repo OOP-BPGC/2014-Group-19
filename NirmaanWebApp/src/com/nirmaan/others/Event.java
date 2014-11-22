e15a6781eebca381457500f1b500c5cae73f4682
Rajat Gupta <f2013464@goa.bits-pilani.ac.in>
2014-11-22 10:31:14 +0530

    Update Event.java

--- NirmaanWebApp/src/com/nirmaan/others/Event.java
+++ NirmaanWebApp/src/com/nirmaan/others/Event.java
@@ -6,6 +6,11 @@ import java.util.*;
 import com.nirmaan.people.Member;
 import com.nirmaan.people.Volunteer;
 
+/**
+ * @author Group-19
+ *Class hsndles the events 
+ *Inner class- Activity
+ */
 public class Event {
 
 	private String name;
@@ -15,54 +20,95 @@ public class Event {
 	private ArrayList<Member> memberList;
 	private ArrayList<Volunteer> volunteerList;
 
+	/**
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name setName
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return Date
+	 */
 	public Date getStartDate() {
 		return startDate;
 	}
 
+	/**
+	 * @param startDate startDate
+	 */
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
 
+	/**
+	 * @return endDate
+	 */
 	public Date getEndDate() {
 		return endDate;
 	}
 
+	/**
+	 * @param endDate endDate
+	 */
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
 
+	/**
+	 * @return activityList
+	 */
 	public ArrayList<Activity> getActivityList() {
 		return activityList;
 	}
 
+	/** 
+	 * @param activityList activityList
+	 */
 	public void setActivityList(ArrayList<Activity> activityList) {
 		this.activityList = activityList;
 	}
 
+	/**
+	 * @return memberList
+	 */
 	public ArrayList<Member> getMemberList() {
 		return memberList;
 	}
 
+	/**
+	 * @param memberList memberList
+	 */
 	public void setMemberList(ArrayList<Member> memberList) {
 		this.memberList = memberList;
 	}
 
+	/**
+	 * @return volunteerList
+	 */
 	public ArrayList<Volunteer> getVolunteerList() {
 		return volunteerList;
 	}
 
+	/**
+	 * @param volunteerList volunteerList
+	 */
 	public void setVolunteerList(ArrayList<Volunteer> volunteerList) {
 		this.volunteerList = volunteerList;
 	}
 
+	/**
+	 * @param n name
+	 * @param sd startDate
+	 * @param ed endDate
+	 */
 	public Event(String n, Date sd, Date ed) {
 		this.name = n;
 		this.startDate = sd;
@@ -70,6 +116,10 @@ public class Event {
 	}
 }
 
+/**
+ * @author Group-19
+ * Inner class of class Event
+ */
 class Activity {
 
 	private String name;
@@ -80,6 +130,13 @@ class Activity {
 	private ArrayList<Volunteer> volunteerList;
 	private String venue;
 
+	/**
+	 * @param n name
+	 * @param v venue
+	 * @param d date
+	 * @param st startDate
+	 * @param et endDate
+	 */
 	public Activity(String n, String v, Date d, Time st, Time et) {
 		this.name = n;
 		this.venue = v;
