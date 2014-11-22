0fa011d27790ef082368df188dd2bdfe20726f3c
Rajat Gupta <f2013464@goa.bits-pilani.ac.in>
2014-11-22 10:32:33 +0530

    Update Meeting.java

--- NirmaanWebApp/src/com/nirmaan/others/Meeting.java
+++ NirmaanWebApp/src/com/nirmaan/others/Meeting.java
@@ -2,12 +2,22 @@ package com.nirmaan.others;
 import java.sql.Time;
 import java.util.Date;
 
+/**
+ * @author Group-19
+ *
+ */
 public class Meeting {
 	private Time startTime;
 	private Time endTime;
 	private Date date;
 	private String venue;
 
+	/**
+	 * @param v venue
+	 * @param d date
+	 * @param st start time
+	 * @param et end time
+	 */
 	public Meeting(String v, Date d, Time st, Time et) {
 		this.venue = v;
 		this.date = d;
